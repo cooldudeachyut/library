1008a29ef93064e50a9b9d2b25afc1ea71b9ef85
Achyut Dwivedi <cooldudeachyut@gmail.com>
2021-07-07 17:14:01 +0530

    Adds grid UI to show added books as cards

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+let myLibrary = [];
+
+function Book (title, author, year, publisher, genre) {
+	this.title = title;
+	this.year = year;
+	this.author = author;
+	this.publisher = publisher;
+	this.genre = genre;
+}
+
+function addBookToLibrary() {
+	
+}
